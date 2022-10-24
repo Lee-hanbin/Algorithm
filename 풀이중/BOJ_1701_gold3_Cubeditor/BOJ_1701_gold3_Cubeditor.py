c787dea39a1a043b606c3ff1032d2bbf13552d5c
Lee-hanbin <hanbin0501@naver.com>
2022-10-25 00:35:21 +0900

    upload boj

--- 풀이중/BOJ_1701_gold3_Cubeditor/BOJ_1701_gold3_Cubeditor.py
+++ 풀이중/BOJ_1701_gold3_Cubeditor/BOJ_1701_gold3_Cubeditor.py
@@ -1,10 +1,22 @@
 # BOJ_1701_gold3_Cubeditor
 
-T = input().strip()
-l = len(T)
-k = 0
-while k < l: 
-    for i in range(k+1):
-        chk = T[i:l-k+i]
-        print(chk)
-    k += 1
+import sys
+input = sys.stdin.readline
+
+def pre_process(P):
+    next = [0] * len(P)
+    j = 0
+    for i in range(1, len(P)):
+        while j > 0 and P[i] != P[j]:
+            j = next[j-1]
+        if P[i] == P[j]:
+            next[i] = j + 1
+            j += 1
+    return next
+ans = 0
+string = input().strip()
+l = len(string)
+for i in range(l):
+    ans = max(ans, max(pre_process(string[i:])))
+
+print(ans)
