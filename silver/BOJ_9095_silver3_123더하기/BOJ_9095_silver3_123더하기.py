4fe0e4c971de41f3508258623ef05cdc5034447f
Lee-hanbin <hanbin0501@naver.com>
2022-11-12 01:58:21 +0900

    upload boj

--- silver/BOJ_9095_silver3_123더하기/BOJ_9095_silver3_123더하기.py
+++ silver/BOJ_9095_silver3_123더하기/BOJ_9095_silver3_123더하기.py
@@ -1,14 +1,18 @@
 # BOJ_9095_silver3_123더하기
 
-lst=[]
-for _ in range(int(input())):
-    lst.append(int(input()))
-K=max(lst)
+n = int(input())
+lst = [0] * n 
+K = 0
+for i in range(n):
+	m = int(input())
+	lst[i] = m
+	if K < m:
+	    K = m
 dp=[0]*(K+1)
 dp[1]=1
 if K >1:
 	dp[2]=2
-if K >2:
+if K > 2:
 	dp[3]=4
 for i in range(4,K+1):
 	dp[i]=(dp[i-1]+ dp[i-2]+ dp[i-3])
