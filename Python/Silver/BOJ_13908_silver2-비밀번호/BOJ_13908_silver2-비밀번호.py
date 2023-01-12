f1ba41a6f8a9da9dff7727e9d34c28adc9f5ee8f
Lee-hanbin <hanbin0501@gmail.com>
2023-01-13 01:55:08 +0900

    upload boj 13908 silver2 비밀번호

--- Python/Silver/BOJ_13908_silver2-비밀번호/BOJ_13908_silver2-비밀번호.py
+++ Python/Silver/BOJ_13908_silver2-비밀번호/BOJ_13908_silver2-비밀번호.py
@@ -1,18 +1,22 @@
 # BOJ_13908_silver2-비밀번호
 
 import sys
-from itertools import combinations
+from itertools import product
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
-
-for i in range(m):
-    lst = list(map(int, input().split()))
-
-if n == m:
-    print(len(combinations(lst, n)))
+if m:
+    set_chk = set(map(int, input().split()))
 else:
-    for i in range(9):
-        if i not in lst:
-            lst.append(i)
-set1 = set(lst)
+    set_chk = set()
+
+sol = 0
+# 중복순열로 10개 중에 n개를 중복이 가능하게 뽑고 선견지명에서 찾을 수 없으면 배제
+for i in product(list(range(10)),repeat = n):
+    for j in set_chk:
+        if j not in i:
+            break
+    else:
+        sol += 1
+print(sol)
