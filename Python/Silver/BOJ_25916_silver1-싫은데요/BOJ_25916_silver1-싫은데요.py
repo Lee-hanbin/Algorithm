638a357ffeba312410e5d6ba83fe4eb5383d172f
Lee-hanbin <hanbin0501@gmail.com>
2023-03-01 23:47:03 +0900

    upload BOJ 25916 silver1 싫은데요

--- Python/Silver/BOJ_25916_silver1-싫은데요/BOJ_25916_silver1-싫은데요.py
+++ Python/Silver/BOJ_25916_silver1-싫은데요/BOJ_25916_silver1-싫은데요.py
@@ -1,64 +1,46 @@
 # BOJ_25916_silver1-싫은데요
 
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
 jar = list(map(int, input().split()))
-
-que = deque()
-que.append(jar.pop())
+front, rear = 0, 0
 sol = 0
-while que:
-    # print(que)
-    # print(jar)
-    volume = sum(que)
-    if volume < m:
-        tmp = volume + jar[-1] if jar else volume
+chk = 0
 
-        if tmp < m:
-            if jar:
-                que.append(jar.pop()) 
-            else:
-                sol = max(sol, volume)
-                break
-        elif tmp > m:
-            sol = max(sol, volume)
-            que.popleft()
-            que.append(jar.pop())
-        else:
-            sol = m
-            break
-    elif volume > m:
-        que.popleft()
-        if jar:
-            que.append(jar.pop())
+
+while front < n:
+    # print('front :', front)
+    # print('rear  :', rear)
+    # print('chk   :', chk)
+    # print('sol   :', sol)
+    # print()
+
+    # front와 rear가 같은 경우
+    if front == rear:
+        chk = jar[front]
+        rear += 1
     else:
-        sol = m
-        break
+        # 햄스터의 머리가 항아리 끝이 아닌경우
+        if rear < n:
+            if chk > m:             # 햄스터의 부피보다 큰 경우
+                chk -= jar[front]
+                front += 1
+            else:                   # 햄스터의 부피보다 작은 경우
+                chk += jar[rear]
+                rear += 1
+        # 햄스터의 머리가 항아리 끝인 경우 
+        else:
+            if chk > m:             # 햄스터의 부피보다 큰 경우는 햄스터를 줄임
+                chk -= jar[front]
+                front += 1
+            else:                   # 햄스터의 부피보다 작은 경우, break
+                break
+
+    if chk <= m:
+        sol = max(chk, sol)    
+
 print(sol)
-    
-# if jar.count(m) > 1:
-#     print(m)
-# else:
-#     head, tail = 0, 1
-#     sol = 0
-#     while tail < n+2:
-#         volume = sum(jar[head:tail])
-#         if volume <= m:
-#             tail += 1
-#         else:
-#             tmp = volume - jar[tail-1] 
-#             if tmp <= m:
-#                 sol = max(sol, tmp)
-#             head += 1
-#         # print('volume: ', volume)
-#         # print('haed : ', head)
-#         # print('tail : ', tail)
-#         # print()
-#         if head == tail and tail < n+1:
-#             tail += 1
-#     print(int(sol))
---
+++ Python/Silver/BOJ_25916_silver1-싫은데요/README.md
@@ -0,0 +1,3 @@
+![연습-297.jpg](README_assets/3508ce4ad80217d84637c39dc65bb68deb99ec3a.jpg)
+
+
