6cf972ae91811a6497213969271402fed471afd0
Lee-hanbin <hanbin0501@gmail.com>
2023-03-19 03:09:48 +0900

    upload BOJ 17281 gold4 야구

--- Python/Gold/BOJ_17281_gold4-야구/BOJ_17281_gold4-야구.py
+++ Python/Gold/BOJ_17281_gold4-야구/BOJ_17281_gold4-야구.py
@@ -1,50 +1,56 @@
 # BOJ_17281_gold4-야구
 
 import sys
-from collections import deque
+from itertools import permutations
 
 input = sys.stdin.readline
 
 
-def play(hitter):
+# 야구 시작
+def play(hitter_lst, hitter):
     score = 0
-    cnt = 0
+    # 이닝 만큼 진행
     for inning in range(n):
         out= 0
-        while 1:
-            if out == 3:
-                out = 0
-                break
+        tag1, tag2, tag3 = 0, 0, 0
+        # 3아웃될때까지 이닝 진행
+        while out < 3:
+            # if out == 3:
+            #     out = 0
+            #     break
 
+            # 순서
             hitter = hitter % 9
-            cnt += innings[inning][hitter]
+            # 순서에 해당하는 타자의 타격결과
+            status = innings[inning][hitter_lst[hitter]]
 
-            if innings[inning][hitter]:
-                if cnt // 4:
-                    score += (cnt % 4 + 1)
-                    cnt = 0
-            else:
+            if not status:
                 out += 1
+            elif status == 1:
+                score += tag3
+                tag1, tag2, tag3 = 1, tag1, tag2
+            elif status == 2:
+                score += tag2 + tag3
+                tag1, tag2, tag3 = 0, 1, tag1                
+            elif status == 3:
+                score += tag1 + tag2 + tag3
+                tag1, tag2, tag3 = 0, 0, 1
+            else:
+                score += 1 + tag1 + tag2 + tag3
+                tag1, tag2, tag3 = 0, 0, 0                
 
-            print('hitter, score, cnt, out', hitter, score, cnt, out)   
-            hitter += 1  
-
+            hitter += 1
     return score
-# 24 + 7 + 8 + 8
+
 n = int(input())
 
 innings = [list(map(int, input().split())) for _ in range(n)]
-tmp = [[0] * 9 for _ in range(n)]
+tmp = []
 
 sol = 0
 
-# 첫 타자가 1번타자부터 9번타자까지 
-for start in range(9):
-    chk = (start + 4)
-    for i in range(n):
-        innings[i] = innings[i][1:4] + innings[i][0] + innings[i][4:]
-
-
-    sol = max(sol, play(start))
-
+# 첫 타자가 1번타자부터 9번타자까지 4번 타자는 항상 첫 타자
+for start in permutations([1,2,3,4,5,6,7,8], 8):
+    tmp = list(start)[:3] + [0] + list(start)[3:]
+    sol = max(sol, play(tmp, 0))
 print(sol)
---
+++ Python/Gold/BOJ_17281_gold4-야구/README.md
@@ -0,0 +1,3 @@
+![연습-313.jpg](README_assets/090cc7d4033f47b12af74c040b7fd3dcfe5cf0bb.jpg)
+
+
