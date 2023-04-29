c9d13dba08ed40e06f69d742ae75e544429e04e5
Lee-hanbin <hanbin0501@gmail.com>
2023-04-29 20:58:41 +0900

    upload BOJ 4803 gold4 트리

--- Python/BOJ/Gold/BOJ_4803_gold4-트리/BOJ_4803_gold4-트리.py
+++ Python/BOJ/Gold/BOJ_4803_gold4-트리/BOJ_4803_gold4-트리.py
@@ -2,53 +2,56 @@
 
 import sys
 
-inpurt = sys.stdin.readline
+input = sys.stdin.readline
 
-def find_parents(parent, node):
-    if parent[node] != node:
-        parent[node] = find_parents(parent, parent[node])
-    return parent[node]
-
-def union_parent(parent, a, b):
-    a = find_parents(parent, a)
-    b = find_parents(parent, b)
-    
+def union_parent(a, b):
+    a = find_parents(a)
+    b = find_parents(b)
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
 
+def find_parents(node):
+    if parent[node] != node:
+        parent[node] = find_parents(parent[node])
+    return parent[node]
+
+
 case = 0
 
 while 1:
     n, m = map(int, input().split())
-    parent = [0] * (n + 1)
-    sol_set = set()
-    cycle_set = set()
     
-    if not n:
+    if n==0 and m==0:
         break
     else:
         case += 1
-            
+        sol_set = set()
+        cycle_set = set()
+        parent = [0] * (n + 1)            
         for i in range(1, n+1):
             parent[i] = i
         
         for _ in range(m):
             s, e = map(int, input().split())
             
-            if s in cycle_set or e in cycle_set or find_parents(parent, s) == find_parents(parent, e):
-                cycle_set.add(find_parents(parent, s))
+            if find_parents(s) == find_parents(e):
+                cycle_set.add(parent[s])
             else:
-                union_parent(parent, s, e)
-        
+                union_parent(s, e)
+                if s in cycle_set or e in cycle_set:
+                    cycle_set.add(parent[s])
+
+
+    # find 함수를 쭉 돌려서 각 정점의 parent를 갱신한다.
+    for i in range(n+1):
+        find_parents(i)
+
     sol_set = set(parent)
-    # print(cycle_set)
-    # print(sol_set)
-    
     sol_set = sol_set - cycle_set
     sol = len(sol_set) - 1
-    
+
     if not sol:
         print(f'Case {case}: No trees.')
     elif sol == 1:
---
+++ Python/BOJ/Gold/BOJ_4803_gold4-트리/README.md
@@ -0,0 +1,3 @@
+![연습-349.jpg](README_assets/6db1604baf948b61055a8484c4aa53ffbfad91a5.jpg)
+
+
